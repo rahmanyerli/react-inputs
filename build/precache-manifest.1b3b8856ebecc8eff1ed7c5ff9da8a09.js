248e2134471d149b1f8a64f2313ef995e54ab65b
Rahman Yerli <rahmanyerli@gmail.com>
2019-02-05 00:58:40 +1100

    build

---
+++ build/precache-manifest.1b3b8856ebecc8eff1ed7c5ff9da8a09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "536e554dffc2df7043ef",
+    "url": "/static/js/main.536e554d.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "536e554dffc2df7043ef",
+    "url": "/static/css/main.ff359d06.chunk.css"
+  },
+  {
+    "revision": "bd079f444d14f4a84716d1ea751d627a",
+    "url": "/index.html"
+  }
+];
